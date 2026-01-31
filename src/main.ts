0f12e4f214aab0e22b6e1cbdf09b94067481b61b
Sergey Batukov <sashachepuha@gmail.com>
2026-01-31 13:15:02 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,41 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+import { AddressesModule } from './addresses/addresses.module';
+import { DatabaseModule } from './database/database.module';
 
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    DatabaseModule,
+    AddressesModule,
+  ],
+})
+class AppModule {}
+
+function setupSwagger(app: INestApplication): void {
   const config = new DocumentBuilder()
     .setTitle('Address API')
     .setDescription('API для поиска адресов')
     .setVersion('1.0.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-
-  await app.listen(process.env.PORT ?? 3000);
 }
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, {});
+
+  app.enableShutdownHooks();
+  setupSwagger(app);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+}
+
 void bootstrap();
